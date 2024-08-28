f35593ca0547b3737729d8697f9a99df2e0402d8
a9sk <920a9sk42f76c765@proton.me>
2024-08-28 09:38:08 +0200

    better comments

--- setup.sh
+++ setup.sh
@@ -8,14 +8,17 @@ wget --no-check-certificate https://www.openssl.org/source/openssl-1.0.2u.tar.gz
 openssldir=$(pwd)
 tar -zxf openssl-1.0.2u.tar.gz
 
-echo -e "${GREEN}[+][+][+][+] INSTALLING NGINX [+][+][+][+]${NC}"
+echo -e "${GREEN}[+][+][+][+] DOWNLOADING NGINX [+][+][+][+]${NC}"
 sudo apt-get update
 sudo apt-get install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev libgd-dev libxml2 libxml2-dev uuid-dev
 wget http://nginx.org/download/nginx-1.9.0.tar.gz
 tar -zxvf nginx-1.9.0.tar.gz
 cd nginx-1.9.0
+
+echo -e "${GREEN}[+][+][+][+] CONFIGURING NGINX [+][+][+][+]${NC}"
 ./configure --with-http_ssl_module --with-openssl=$openssldir/openssl-1.0.2u --with-openssl-opt='enable-weak-ssl-ciphers enable-rc4' --with-http_gzip_static_module --prefix=/usr/local/nginx --with-cc-opt="-Wno-error"
 make
 sudo make install
 
+echo -e "${GREEN}[+][+][+][+] CREATING AN ALIAS [+][+][+][+]${NC}"
 echo "alias nginx=\"sudo /usr/local/nginx/sbin/nginx\"" | tee -a ~/.bashrc
