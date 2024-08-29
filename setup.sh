3b02926f60bbc21a931ef203672b5032dc6607b6
Daniel Franchini <73396382+dani3lfrenc@users.noreply.github.com>
2024-08-29 14:30:04 +0200

    added argument for openssl version and version check

--- setup.sh
+++ setup.sh
@@ -3,13 +3,45 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+
+DEFAULT_OPENSSL_VERSION="1.0.1u"
+if [ -z $1 ]; then
+    OPENSSL_VERSION=$DEFAULT_OPENSSL_VERSION
+else
+    OPENSSL_VERSION_LIST=(
+        "1.0.1g"
+        "1.0.1h"
+        "1.0.1i"
+        "1.0.1j"
+        "1.0.1k"
+        "1.0.1l"
+        "1.0.1m"
+        "1.0.1n"
+        "1.0.1o"
+        "1.0.1p"
+        "1.0.1q"
+        "1.0.1r"
+        "1.0.1s"
+        "1.0.1t"
+        "1.0.1u"
+    )    
+
+
+    for version in "${OPENSSL_VERSION_LIST[@]}"; do
+        if [ "$1" == "$version" ]; then
+            OPENSSL_VERSION="$1"
+            break
+        fi
+    done
+fi
+
 echo "Start setup of the virtual machine..."
 
 echo -e "${GREEN}[+][+][+][+] DOWNGRADING OPENSSL [+][+][+][+]${NC}"
-wget --no-check-certificate https://www.openssl.org/source/openssl-1.0.1b.tar.gz
+wget --no-check-certificate https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
 openssldir=$(pwd)
-tar -zxf openssl-1.0.1b.tar.gz
-rm openssl-1.0.1b.tar.gz
+tar -zxf openssl-$OPENSSL_VERSION.tar.gz
+rm openssl-$OPENSSL_VERSION.tar.gz
 
 echo -e "${GREEN}[+][+][+][+] DOWNLOADING NGINX [+][+][+][+]${NC}"
 sudo apt-get update
@@ -20,7 +52,7 @@ rm nginx-1.9.0.tar.gz
 cd nginx-1.9.0
 
 echo -e "${GREEN}[+][+][+][+] CONFIGURING NGINX [+][+][+][+]${NC}"
-./configure --with-http_ssl_module --with-openssl=$openssldir/openssl-1.0.1b --with-openssl-opt='enable-weak-ssl-ciphers enable-rc4 enable-ssl2' --with-http_gzip_static_module --prefix=/usr/local/nginx --with-cc-opt="-Wno-error"
+./configure --with-http_ssl_module --with-openssl=$openssldir/openssl-$OPENSSL_VERSION --with-openssl-opt='enable-weak-ssl-ciphers enable-rc4 enable-ssl2' --with-http_gzip_static_module --prefix=/usr/local/nginx --with-cc-opt="-Wno-error"
 make
 sudo make install
 cd ..
