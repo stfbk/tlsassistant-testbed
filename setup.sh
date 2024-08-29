a9cb28796a81776a10c45821d73039fcda3e6a0d
a9sk <920a9sk42f76c765@proton.me>
2024-08-29 10:56:56 +0200

    better cert generation and downgraded openssl to version 1.0.1a

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ NC='\033[0m' # No Color
 echo "Start setup of the virtual machine..."
 
 echo -e "${GREEN}[+][+][+][+] DOWNGRADING OPENSSL [+][+][+][+]${NC}"
-wget --no-check-certificate https://www.openssl.org/source/openssl-1.0.1u.tar.gz
+wget --no-check-certificate https://www.openssl.org/source/openssl-1.0.1a.tar.gz
 openssldir=$(pwd)
-tar -zxf openssl-1.0.1u.tar.gz
-rm openssl-1.0.1u.tar.gz
+tar -zxf openssl-1.0.1a.tar.gz
+rm openssl-1.0.1a.tar.gz
 
 echo -e "${GREEN}[+][+][+][+] DOWNLOADING NGINX [+][+][+][+]${NC}"
 sudo apt-get update
@@ -20,7 +20,7 @@ rm nginx-1.9.0.tar.gz
 cd nginx-1.9.0
 
 echo -e "${GREEN}[+][+][+][+] CONFIGURING NGINX [+][+][+][+]${NC}"
-./configure --with-http_ssl_module --with-openssl=$openssldir/openssl-1.0.1u --with-openssl-opt='enable-weak-ssl-ciphers enable-rc4 enable-ssl2' --with-http_gzip_static_module --prefix=/usr/local/nginx --with-cc-opt="-Wno-error"
+./configure --with-http_ssl_module --with-openssl=$openssldir/openssl-1.0.1a --with-openssl-opt='enable-weak-ssl-ciphers enable-rc4 enable-ssl2' --with-http_gzip_static_module --prefix=/usr/local/nginx --with-cc-opt="-Wno-error"
 make
 sudo make install
 cd ..
@@ -29,7 +29,7 @@ echo -e "${GREEN}[+][+][+][+] CREATING AN ALIAS [+][+][+][+]${NC}"
 sudo mv nginx /usr/bin
 
 echo -e "${GREEN}[+][+][+][+] GENERATING THE CERTIFICATE [+][+][+][+]${NC}"
-sudo openssl req -x509 -nodes -newkey rsa:4096 -keyout /usr/local/nginx/conf/cert.key -out /usr/local/nginx/conf/cert.pem -days 365
+sudo openssl req -x509 -nodes -subj "/CN=fbk.eu" -newkey rsa:4096 -keyout /usr/local/nginx/conf/cert.key -out /usr/local/nginx/conf/cert.pem -days 365
 
 echo -e "${GREEN}[+][+][+][+] COPY THE SERVER CONFIGURATION [+][+][+][+]${NC}"
 sudo cp config.conf /usr/local/nginx/conf/nginx.conf
