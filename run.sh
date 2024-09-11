0eb715d75996a094ef62a5375727581e6cb21cf3
a9sk <920a9sk42f76c765@proton.me>
2024-09-11 10:33:00 +0200

    full configuration of Nginx 1.9.0 WebServer with OpenSSL version 1.0.1a

--- run.sh
+++ run.sh
@@ -26,3 +26,27 @@ sudo cp ../configs/config-1.0.1u.conf /usr/local/nginx-1.0.1u/conf/nginx.conf
 
 echo -e "${GREEN}[+][+][+][+] STARTING THE WEBSERVER WITH NGINX WITH OPENSSL VERSION 1.0.1u [+][+][+][+]${NC}"
 sudo /usr/local/nginx-1.0.1u/sbin/nginx
+
+# full configuration of Nginx 1.9.0 WebServer with OpenSSL version 1.0.1a
+
+echo -e "${GREEN}[+][+][+][+] DOWNLOADING OPENSSL VERSION 1.0.1a [+][+][+][+]${NC}"
+wget --no-check-certificate https://www.openssl.org/source/openssl-1.0.1a.tar.gz
+tar -zxf openssl-1.0.1a.tar.gz
+rm openssl-1.0.1a.tar.gz
+
+echo -e "${GREEN}[+][+][+][+] FIXING OPENSSL DOCS [+][+][+][+]${NC}"
+sudo cp -rf ../doc openssl-1.0.1a
+
+echo -e "${GREEN}[+][+][+][+] CONFIGURING NGINX WITH OPENSSL VERSION 1.0.1a [+][+][+][+]${NC}"
+./configure --with-http_ssl_module --with-openssl="openssl-1.0.1a" --with-openssl-opt='enable-weak-ssl-ciphers enable-rc4 enable-ssl2' --with-http_gzip_static_module --prefix=/usr/local/nginx-1.0.1a --with-cc-opt="-Wno-error"
+sudo make
+sudo make install
+
+echo -e "${GREEN}[+][+][+][+] GENERATING THE CERTIFICATE FOR NGINX WITH OPENSSL VERSION 1.0.1a [+][+][+][+]${NC}"
+sudo openssl req -x509 -nodes -subj "/CN=fbk.eu" -newkey rsa:4096 -keyout /usr/local/nginx-1.0.1a/conf/cert.key -out /usr/local/nginx-1.0.1a/conf/cert.pem -days 365
+
+echo -e "${GREEN}[+][+][+][+] COPYING THE SERVER CONFIGURATION [+][+][+][+]${NC}"
+sudo cp ../configs/config-1.0.1a.conf /usr/local/nginx-1.0.1a/conf/nginx.conf
+
+echo -e "${GREEN}[+][+][+][+] STARTING THE WEBSERVER WITH NGINX WITH OPENSSL VERSION 1.0.1a [+][+][+][+]${NC}"
+sudo /usr/local/nginx-1.0.1a/sbin/nginx
