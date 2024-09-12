017fe797257c2d037627679499c96fb49835d25d
dani3lfrenc <danielfranchini@virgilio.it>
2024-09-12 17:02:46 +0200

    fix and ottimization

--- run.sh
+++ run.sh
@@ -8,7 +8,10 @@ NC='\033[0m' # No Color
 echo -e "${GREEN}[+][+][+][+] Start setup of the virtual machine... [+][+][+][+]${NC}"
 
 sudo apt-get update
-sudo apt-get -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev libgd-dev libxml2 libxml2-dev uuid-dev git docker.io
+sudo apt-get -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev libgd-dev libxml2 libxml2-dev uuid-dev git docker.io 
+sudo apt install -y aha html2text libxml2-utils pandoc dos2unix python-pip libexpat1-dev geany 
+sudo pip install --pre tlslite-ng
+
 
 echo -e "${GREEN}[+][+][+][+] DOWNLOADING NGINX VERSION 1.9.0 [+][+][+][+]${NC}"
 wget http://nginx.org/download/nginx-1.9.0.tar.gz
@@ -64,42 +67,29 @@ cd ..
 
 # full configuration of Apache Webserver with apr-1.6.5, apr-util-1.6.1, httpd 2.4.37 and OpenSSL version 1.0.2-stable
 mkdir apache && cd apache
-echo -e "${GREEN}[+][+][+][+] INSTALLING DEPENDENCIES FOR APACHE WITH OPENSSL 1.0.2 [+][+][+][+]${NC}"
-sudo apt install -y aha html2text libxml2-utils pandoc dos2unix python-pip
-pip install --pre tlslite-ng
-sudo apt install -y libexpat1-dev
-sudo apt install -y python-pip
-pip install --pre tlslite-ng
-sudo apt install -y pandoc geany dos2unix
-
 echo -e "${GREEN}[+][+][+][+] DOWNLOADING OPENSSL VERSION 1.0.2 [+][+][+][+]${NC}"
 git clone -b OpenSSL_1_0_2-stable https://github.com/openssl/openssl.git
 cd openssl
 ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl enable-weak-ssl-ciphers enable-deprecated enable-rc4 enable-ssl2 enable-ssl3 enable-ssl3-method enable-comp enable-zlib-dynamic -Wl,-rpath=/usr/local/ssl/lib
 make depend
 make 
-sudo make install #sw
+sudo make install 
 cd ..
 
 echo -e "${GREEN}[+][+][+][+] DOWNLOADING APACHE HTTPD, APR & APR-UTIL [+][+][+][+]${NC}"
-wget http://mirror.nohup.it/apache//httpd/httpd-2.4.37.tar.bz2
-tar xvjf httpd-2.4.37.tar.bz2
-rm httpd-2.4.37.tar.bz2
 
-wget http://it.apache.contactlab.it//apr/apr-1.6.5.tar.gz
-tar xvf apr-1.6.5.tar.gz
-rm apr-1.6.5.tar.gz
+tar xvjf ../dependencies/httpd-2.4.37.tar.bz2
+
+tar xvf ../dependencies/apr-1.6.5.tar.gz
 mv apr-1.6.5 httpd-2.4.37/srclib/apr
 
-wget http://it.apache.contactlab.it//apr/apr-util-1.6.1.tar.gz
-tar xvf apr-util-1.6.1.tar.gz
-rm apr-util-1.6.1.tar.gz
+tar xvf ../dependencies/apr-util-1.6.1.tar.gz
 mv apr-util-1.6.1 httpd-2.4.37/srclib/apr-util
 
 cd httpd-2.4.37
 ./configure --with-included-apr --enable-ssl --with-ssl=/usr/local/ssl --enable-deflate --enable-mods-static=ssl --enable-mods-shared=deflate
 make
-sudo make install #sw
+sudo make install
 cd ..
 
 echo -e "${GREEN}[+][+][+][+] GENERATING THE CERTIFICATE FOR APACHE WITH OPENSSL VERSION 1.0.2 [+][+][+][+]${NC}"
@@ -107,8 +97,8 @@ mkdir certificates && cd certificates
 sudo openssl genrsa -out dummy.com.key 4096
 sudo openssl req -new -key dummy.com.key -out dummy.com.csr -sha512 -subj '/C=IT/ST=Trento/L=Trento/O=FBK/OU=S&T/CN=www.dummy.com'
 sudo openssl x509 -req -days 365 -in dummy.com.csr -signkey dummy.com.key -out dummy.com.crt -sha512
-cd ..
-cp certificates  /usr/local/apache2/conf/extra/
+cd .. 
+cp -r certificates/ /usr/local/apache2/
 
 
 echo -e "${GREEN}[+][+][+][+] COPYING THE SERVER CONFIGURATION [+][+][+][+]${NC}"
@@ -118,6 +108,8 @@ sudo rm /usr/local/apache2/htdocs/index.html 2>/dev/null
 sudo touch /usr/local/apache2/htdocs/index.html
 echo "<html><head><title>Experiment</title></head><hr><h1>Experimental dummy page</h1><p>Dummy text, from dummy developers, for dummy code. ;) </p><!-- DummyDevs ;) --></body></html>" | sudo tee -a /usr/local/apache2/htdocs/index.html > /dev/null
 cd ..
+
+sudo /usr/local/apache2/bin/apachectl -k start
 
 # full configuration of OpenSSL S_Server with OpenSSL version 1.0.2-patched by DamnVulnerableOpenSSL (https://github.com/tls-attacker/DamnVulnerableOpenSSL.git)
 
@@ -130,3 +122,7 @@ sudo docker build -t damnvulnerableopenssl .
 
 echo -e "${GREEN}[+][+][+][+] RUNNING DamnVulnerableOpenSSL OPENSSL SERVER [+][+][+][+]${NC}"
 sudo docker run -p 9006:9006 damnvulnerableopenssl &
+
+
+AH00526: Syntax error on line 144 of /usr/local/apache2/conf/extra/httpd-ssl.conf:
+SSLCertificateFile: file '/usr/local/apache2/certificates/dummy.com.crt' does not exist
