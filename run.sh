23b675f1916fd1898a8c03c3a4665973b125228a
a9sk <920a9sk42f76c765@proton.me>
2024-09-13 10:26:45 +0200

    removed unnecessary sudos

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ NC='\033[0m' # No Color
 
 echo -e "${GREEN}[+][+][+][+] Start setup of the virtual machine... [+][+][+][+]${NC}"
 
-sudo apt-get update
-sudo apt-get -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev libgd-dev libxml2 libxml2-dev uuid-dev git docker.io
-sudo apt install -y aha html2text libxml2-utils pandoc dos2unix python-pip libexpat1-dev geany
-sudo pip install --pre tlslite-ng
+apt-get update
+apt-get -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev libgd-dev libxml2 libxml2-dev uuid-dev git docker.io
+apt install -y aha html2text libxml2-utils pandoc dos2unix python-pip libexpat1-dev geany
+pip install --pre tlslite-ng
 
 
 echo -e "${GREEN}[+][+][+][+] DOWNLOADING NGINX VERSION 1.9.0 [+][+][+][+]${NC}"
@@ -28,17 +28,17 @@ rm openssl-1.0.1u.tar.gz
 
 echo -e "${GREEN}[+][+][+][+] CONFIGURING NGINX WITH OPENSSL VERSION 1.0.1u [+][+][+][+]${NC}"
 ./configure --with-http_ssl_module --with-openssl="openssl-1.0.1u" --with-openssl-opt='enable-weak-ssl-ciphers enable-rc4 enable-ssl2' --with-http_gzip_static_module --prefix=/usr/local/nginx-1.0.1u --with-cc-opt="-Wno-error"
-sudo make
-sudo make install
+make
+make install
 
 echo -e "${GREEN}[+][+][+][+] GENERATING THE CERTIFICATE FOR NGINX WITH OPENSSL VERSION 1.0.1u [+][+][+][+]${NC}"
-sudo openssl req -x509 -nodes -subj "/CN=fbk.eu" -newkey rsa:4096 -keyout /usr/local/nginx-1.0.1u/conf/cert.key -out /usr/local/nginx-1.0.1u/conf/cert.pem -days 365
+openssl req -x509 -nodes -subj "/CN=fbk.eu" -newkey rsa:4096 -keyout /usr/local/nginx-1.0.1u/conf/cert.key -out /usr/local/nginx-1.0.1u/conf/cert.pem -days 365
 
 echo -e "${GREEN}[+][+][+][+] COPYING THE SERVER CONFIGURATION [+][+][+][+]${NC}"
-sudo cp ../configs/config-1.0.1u.conf /usr/local/nginx-1.0.1u/conf/nginx.conf
+cp ../configs/config-1.0.1u.conf /usr/local/nginx-1.0.1u/conf/nginx.conf
 
 echo -e "${GREEN}[+][+][+][+] STARTING THE WEBSERVER WITH NGINX WITH OPENSSL VERSION 1.0.1u [+][+][+][+]${NC}"
-sudo /usr/local/nginx-1.0.1u/sbin/nginx
+/usr/local/nginx-1.0.1u/sbin/nginx
 
 # full configuration of Nginx 1.9.0 WebServer with OpenSSL version 1.0.1a
 
@@ -48,21 +48,21 @@ tar -zxf openssl-1.0.1a.tar.gz
 rm openssl-1.0.1a.tar.gz
 
 echo -e "${GREEN}[+][+][+][+] FIXING OPENSSL DOCS [+][+][+][+]${NC}"
-sudo cp -rf ../doc openssl-1.0.1a
+cp -rf ../doc openssl-1.0.1a
 
 echo -e "${GREEN}[+][+][+][+] CONFIGURING NGINX WITH OPENSSL VERSION 1.0.1a [+][+][+][+]${NC}"
 ./configure --with-http_ssl_module --with-openssl="openssl-1.0.1a" --with-openssl-opt='enable-weak-ssl-ciphers enable-rc4 enable-ssl2' --with-http_gzip_static_module --prefix=/usr/local/nginx-1.0.1a --with-cc-opt="-Wno-error"
-sudo make
-sudo make install
+make
+make install
 
 echo -e "${GREEN}[+][+][+][+] GENERATING THE CERTIFICATE FOR NGINX WITH OPENSSL VERSION 1.0.1a [+][+][+][+]${NC}"
-sudo openssl req -x509 -nodes -subj "/CN=fbk.eu" -newkey rsa:4096 -keyout /usr/local/nginx-1.0.1a/conf/cert.key -out /usr/local/nginx-1.0.1a/conf/cert.pem -days 365
+openssl req -x509 -nodes -subj "/CN=fbk.eu" -newkey rsa:4096 -keyout /usr/local/nginx-1.0.1a/conf/cert.key -out /usr/local/nginx-1.0.1a/conf/cert.pem -days 365
 
 echo -e "${GREEN}[+][+][+][+] COPYING THE SERVER CONFIGURATION [+][+][+][+]${NC}"
-sudo cp ../configs/config-1.0.1a.conf /usr/local/nginx-1.0.1a/conf/nginx.conf
+cp ../configs/config-1.0.1a.conf /usr/local/nginx-1.0.1a/conf/nginx.conf
 
 echo -e "${GREEN}[+][+][+][+] STARTING THE WEBSERVER WITH NGINX WITH OPENSSL VERSION 1.0.1a [+][+][+][+]${NC}"
-sudo /usr/local/nginx-1.0.1a/sbin/nginx
+/usr/local/nginx-1.0.1a/sbin/nginx
 cd ..
 
 # full configuration of Apache Webserver with apr-1.6.5, apr-util-1.6.1, httpd 2.4.37 and OpenSSL version 1.0.2-stable
@@ -73,7 +73,7 @@ cd openssl
 ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl enable-weak-ssl-ciphers enable-deprecated enable-rc4 enable-ssl2 enable-ssl3 enable-ssl3-method enable-comp enable-zlib-dynamic -Wl,-rpath=/usr/local/ssl/lib
 make depend
 make
-sudo make install_sw
+make install_sw
 cd ..
 
 echo -e "${GREEN}[+][+][+][+] DOWNLOADING APACHE HTTPD, APR & APR-UTIL [+][+][+][+]${NC}"
@@ -89,27 +89,27 @@ mv apr-util-1.6.1 httpd-2.4.37/srclib/apr-util
 cd httpd-2.4.37
 ./configure --with-included-apr --enable-ssl --with-ssl=/usr/local/ssl --enable-deflate --enable-mods-static=ssl --enable-mods-shared=deflate
 make
-sudo make install
+make install
 cd ..
 
 echo -e "${GREEN}[+][+][+][+] GENERATING THE CERTIFICATE FOR APACHE WITH OPENSSL VERSION 1.0.2 [+][+][+][+]${NC}"
 mkdir certificates && cd certificates
-sudo openssl genrsa -out dummy.com.key 4096
-sudo openssl req -new -key dummy.com.key -out dummy.com.csr -sha512 -subj '/C=IT/ST=Trento/L=Trento/O=FBK/OU=S&T/CN=www.dummy.com'
-sudo openssl x509 -req -days 365 -in dummy.com.csr -signkey dummy.com.key -out dummy.com.crt -sha512
+openssl genrsa -out dummy.com.key 4096
+openssl req -new -key dummy.com.key -out dummy.com.csr -sha512 -subj '/C=IT/ST=Trento/L=Trento/O=FBK/OU=S&T/CN=www.dummy.com'
+openssl x509 -req -days 365 -in dummy.com.csr -signkey dummy.com.key -out dummy.com.crt -sha512
 cd ..
 cp -r certificates/ /usr/local/apache2/
 
 
 echo -e "${GREEN}[+][+][+][+] COPYING THE SERVER CONFIGURATION [+][+][+][+]${NC}"
-sudo cp ../configs/httpd-ssl-apache.conf /usr/local/apache2/conf/extra/httpd-ssl.conf
-sudo cp ../configs/httpd-apache.conf /usr/local/apache2/conf/httpd.conf
-sudo rm /usr/local/apache2/htdocs/index.html 2>/dev/null
-sudo touch /usr/local/apache2/htdocs/index.html
-echo "<html><head><title>Experiment</title></head><hr><h1>Experimental dummy page</h1><p>Dummy text, from dummy developers, for dummy code. ;) </p><!-- DummyDevs ;) --></body></html>" | sudo tee -a /usr/local/apache2/htdocs/index.html > /dev/null
+cp ../configs/httpd-ssl-apache.conf /usr/local/apache2/conf/extra/httpd-ssl.conf
+cp ../configs/httpd-apache.conf /usr/local/apache2/conf/httpd.conf
+rm /usr/local/apache2/htdocs/index.html 2>/dev/null
+touch /usr/local/apache2/htdocs/index.html
+echo "<html><head><title>Experiment</title></head><hr><h1>Experimental dummy page</h1><p>Dummy text, from dummy developers, for dummy code. ;) </p><!-- DummyDevs ;) --></body></html>" | tee -a /usr/local/apache2/htdocs/index.html > /dev/null
 cd ..
 
-sudo /usr/local/apache2/bin/apachectl -k start
+/usr/local/apache2/bin/apachectl -k start
 
 # full configuration of OpenSSL S_Server with OpenSSL version 1.0.2-patched by DamnVulnerableOpenSSL (https://github.com/tls-attacker/DamnVulnerableOpenSSL.git)
 
@@ -118,7 +118,7 @@ git clone https://github.com/tls-attacker/DamnVulnerableOpenSSL.git
 cd DamnVulnerableOpenSSL
 
 echo -e "${GREEN}[+][+][+][+] BUILDING DOCKER FOR DamnVulnerableOpenSSL [+][+][+][+]${NC}"
-sudo docker build -t damnvulnerableopenssl .
+docker build -t damnvulnerableopenssl .
 
 echo -e "${GREEN}[+][+][+][+] RUNNING DamnVulnerableOpenSSL OPENSSL SERVER [+][+][+][+]${NC}"
-sudo docker run -p 9006:9006 damnvulnerableopenssl &
+docker run -p 9006:9006 damnvulnerableopenssl &
